ceb54159b3cb5c9415856e1b846e5fadf3c7b666
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-06-09 21:06:36 -0400

    update metabox for single page data

--- app/public/wp-content/themes/fictional-university-theme/single.php
+++ app/public/wp-content/themes/fictional-university-theme/single.php
@@ -15,6 +15,18 @@ while(have_posts()) {
       </div>
     </div>
 
+    <div class="container container--narrow page-section">
+    <div class="container container--narrow page-section">
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home </a> <span class="metabox__main">Post by <?php the_author_posts_link(); ?> on <?php the_time('D, M jS, Y'); ?> in <?php echo get_the_category_list(' ,');?></span>
+        </p>
+      </div>
+        <div class="generic-content">
+            <?php the_content(); ?>
+        </div>
+    </div>
+
 <?php }
 
 get_footer();

6389a5748e4ac56bfbcc10e1815e8b055542e65b
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-06-14 21:39:59 -0400

    update event pages and archive. update custom slug for events

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -2,6 +2,10 @@
 function university_post_types()
 {
     register_post_type('event', array(
+        'rewrite' => array(
+            'slug' => 'events'
+        ),
+        'has_archive' => true,
         'public' => true,
         'show_in_rest' => true,
         'menu_icon' => 'dashicons-calendar-alt',

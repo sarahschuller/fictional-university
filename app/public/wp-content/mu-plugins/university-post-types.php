6d1cfe7c5db18619c98785b28dbe40b0a6f9feeb
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-07-16 16:01:05 -0400

    create Program post type and page to view all programs and individual programs

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php
 function university_post_types()
 {
+    // Event Post Type
     register_post_type('event', array(
         'show_in_rest' => true,
         'supports' => array('title', 'editor', 'excerpt'),
@@ -10,14 +11,34 @@ function university_post_types()
         'has_archive' => true,
         'public' => true,
         'show_in_rest' => true,
-        'menu_icon' => 'dashicons-calendar-alt',
         'labels' => array(
             'name' => 'Events',
             'add_new_item' => 'Add New Event',
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
             'singular_name' => 'Event'
-        )
+        ),
+        'menu_icon' => 'dashicons-calendar-alt'
+    ));
+
+    // Program Post Type
+    register_post_type('program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array(
+            'slug' => 'programs'
+        ),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
     ));
 }
 add_action('init', 'university_post_types');
